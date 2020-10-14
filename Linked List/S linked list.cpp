ef52ac45a29fef992ca96a1ffa8a8edb93393cfe
Prabal Jain <prabaljainn@gmail.com>
2020-10-14 21:41:22 +0530

    Thanks

---
+++ Linked List/S linked list.cpp
@@ -0,0 +1,85 @@
+//WAP of LL with insertion, deletion, del LL, get nth node of LL, Lsearch of LL//
+#include<bits/stdc++.h>
+using namespace std;
+class Nodelist
+{public:
+    int data;
+    Nodelist* next;
+};
+void LLPrint(Nodelist** Head){
+Nodelist* t=*Head;
+cout<<"|";
+while(t->next!=NULL){
+    cout<<t->data<<"|--|";
+    t=t->next;
+}cout<<t->data<<"|";
+
+}
+void LLPush(Nodelist** Head, int data){
+Nodelist* temp= new Nodelist();
+temp->data=data;
+temp->next=*Head;
+*Head=temp;
+}
+void LLinsert(Nodelist** Head, int index, int data){
+    Nodelist* temp= new Nodelist();
+temp->data=data;
+temp->next=*Head;
+if(index==1){
+*Head=temp;
+
+}
+else{
+Nodelist*p =*Head;
+for(int i=0;i<index-2;i++){p=p->next;}
+temp->next=p->next;
+p->next=temp;
+
+}
+}
+void LLDelete(Nodelist** Head){
+Nodelist* current = *Head;
+Nodelist* next;
+
+while (current != NULL)
+{
+    next = current->next;
+    free(current);
+    current = next;
+}
+
+*Head = NULL;
+}
+int LLSearch(Nodelist** Head, int key){
+Nodelist* Rotr= *Head;
+int count=1;
+while(Rotr->next!=NULL){
+    if(key==Rotr->data) return count;
+    else {Rotr=Rotr->next;count++;}
+    }
+    return -1;
+
+}
+int LLGetlen(Nodelist** Head){
+int count=0;
+Nodelist*p=*Head;while(1){
+if(p->next==NULL){return count+1;}
+while(p->next!=NULL){count++;p=p->next;}
+}
+}
+
+int main(){
+Nodelist* Head=NULL;
+LLPush(&Head, 2);
+LLPush(&Head, 3);
+LLPush(&Head, 5);
+LLPrint(&Head); cout<<endl;
+LLinsert(&Head,1,7);
+LLinsert(&Head,3,11);
+LLinsert(&Head,2,13);
+LLPrint(&Head); cout<<endl;
+LLPush(&Head, 17);
+LLPrint(&Head); cout<<endl;
+cout<<LLSearch(&Head,11)<<endl;
+cout<<LLGetlen(&Head)<<endl;
+return 0;}
